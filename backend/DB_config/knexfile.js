539f7ff6303dd05f190f141dd2e9d421c55931ac
Cr4zyCRO <bruno.galic02@gmail.com>
2025-11-24 14:42:11 +0100

    Knex configuration

--- backend/DB_config/knexfile.js
+++ backend/DB_config/knexfile.js
@@ -1,47 +1,32 @@
-// Update with your config settings.
+require('dotenv').config();
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+export const development = {
+  client: 'sqlite3',
+  connection: {
+    filename: './dev.sqlite3',
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+};
+export const staging = {
+  client: 'postgresql',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+  pool: { min: 2, max: 10 },
+  migrations: { tableName: 'knex_migrations' },
+};
+export const production = {
+  client: 'postgresql',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  pool: { min: 2, max: 10 },
+  migrations: { tableName: 'knex_migrations' },
 };
