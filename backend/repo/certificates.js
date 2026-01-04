8b528e6f272bff49b1eec7e917cc49714cdcd64a
fmi <filip.milicevic@emovis.hr>
2026-01-04 22:16:43 +0100

    added get certificate content and other minor fixes

--- backend/repo/certificates.js
+++ backend/repo/certificates.js
@@ -2,8 +2,31 @@ import db from "../DB_config/knex.js";
 
 export async function getUserCertificates(userId) {
     const certificates = await db('certificates')
-        .where('userId', userId)
+        .where('student_id', userId)
         .first();
 
     return certificates || null;
+}
+
+export async function getUserCertificate(certificateId) {
+    const certificate = await db('certificates')
+    .where('id', certificateId)
+    .first();
+
+    return certificate || null;
+}
+
+export function base64ToPdfBuffer(base64String) {
+  if (!base64String) throw new Error("Missing base64 content");
+
+  const cleaned = base64String.includes("base64,")
+    ? base64String.split("base64,")[1]
+    : base64String;
+
+  const buf = Buffer.from(cleaned, "base64");
+  if (buf.length < 4 || buf.toString("utf8", 0, 4) !== "%PDF") {
+    throw new Error("Decoded content does not look like a PDF");
+  }
+
+  return buf;
 }
--- backend/route/certificates.js
+++ backend/route/certificates.js
@@ -1,9 +1,9 @@
 import express from "express";
 import Joi from "joi";
-//import { getActiveCompanies, getCompanyById } from "../repo/companies.js";
 import CompanyRepo from "../repo/companies.js";
 import { authorizeStudent, jwtCheck } from "../middleware/authMiddleware.js"; // Middleware za provjeru JWT-a
 import { query, params } from "../middleware/validate.js";         // Middleware za Joi validaciju
+import { base64ToPdfBuffer, getUserCertificate } from "../repo/certificates.js";
 
 const router = express.Router();
 
@@ -15,15 +15,59 @@ router.get(
     async (req, res) => {
         try {
             
-            const result = await getUserCertificates(req.user.id); // this req.user.id should return currently logged in user certificates
+            const result = await getUserCertificates(req.user.id);
                             
-            res.json(result);
+            res.json({
+                certificateId: result.id,
+                name: result.certificate_name,
+                status: result.status,
+                studentId: result.student_id,
+                applicationId: result.application_id
+            });
         } catch (err) {
-            console.error("Error fetching companies:", err.message);
+            console.error("Error fetching certificates:", err.message);
             res.status(500).json({ error: "Failed to fetch user certificates list" });
         }
     }
 )
 
+
+router.get(
+    "/:certificateId",
+    jwtCheck,
+    async (req, res) => {
+
+        const {certificateId} = req.params;
+
+        if (certificateId === null)
+        {
+            console.error("Error fetching certificates:", err.message);
+            res.status(500).json({ error: `CertificateId cannot be null` });
+        }
+
+        try {
+
+        const certificate = await getUserCertificate(certificateId);
+
+        if (certificate === null)
+        {
+            console.error("Error fetching certificates:", err.message);
+            res.status(404).json({ error: `Certificate with id ${certificateId} not found` });
+        }
+
+        const pdfBuffer = base64ToPdfBuffer(certificate.content);
+
+        res.setHeader("Content-Type", "application/pdf");
+        res.setHeader("Content-Disposition", `inline; filename="${certificate.name ?? "file.pdf"}"`);
+        res.setHeader("Content-Length", pdfBuffer.length);
+
+        return res.end(pdfBuffer);
+            
+        } catch (error) {
+            console.error("Error fetching certificates:", err.message);
+            res.status(500).json({ error: `Failed to fetch certificate with id ${certificateId}` });
+        }
+    }
+)
 
 export default router;
